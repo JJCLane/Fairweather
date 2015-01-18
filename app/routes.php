8e021340a1562b45209fe246c186a492d73e2469
Jordan Lane - Laptop <jordan@softsquared.co.uk>
2015-01-18 18:59:06 +0000

    Initial laravel commit

---
+++ app/routes.php
@@ -0,0 +1,26 @@
+<?php
+
+use Fairweather\Repository;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	
+	$facebookRepository = App::make('Fairweather\Repositories\FacebookRepository');
+	$photos = $facebookRepository->getPhotos();
+
+	foreach ($photos->getPropertyAsArray('data') as $photo) {
+		echo '<img src="' . $photo->getproperty('source') . '" />';
+	}
+	//return View::make('hello');
+});
