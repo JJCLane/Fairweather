2a662dda923dfa6826ab945e9dc8f2a43a70d9d5
Jordan Lane - Laptop <jordan@softsquared.co.uk>
2015-01-19 01:53:09 +0000

    Create home and master view

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+ 
+<head>
+	
+	<meta charset="utf-8">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title>Noah - Stein IAS' Web Development Template</title>
+	
+	<script>document.documentElement.className += ' advanced';</script>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<link rel="stylesheet" href="/css/style.css" media="screen">
+	<link href='http://fonts.googleapis.com/css?family=Neuton|Montserrat' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+	<header class="header">
+		<div class="container">
+
+			<nav class="header__nav">
+				<a href="#info">Info</a><!--
+				--><a href="#services">Services</a><!--
+				--><a href="#testimonials">Testimonials</a><!--
+				--><a href="#contact">Contact</a>
+			</nav>
+
+			<div class="wrapper">
+
+				<img class="header__img header__img--leaf" src="/img/leaf-background.jpg" alt="The leaf part of the logo with image of forest" width="469" height="536"/>
+
+				<img class="header__img header__img--logo" src="/img/fairweather-logo.png" alt="Fairweather Garden Services logo" width="403" height="536"/>
+			</div>
+		</div>
+	</header>
+
+	<section>
+		<div class="container">
+			
+			<h2>Info</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate, nunc ut tincidunt fermentum, est leo accumsan risus, ut aliquam eros justo eget est. Quisque dui est, imperdiet iaculis interdum ut, posuere at purus. Suspendisse potenti. Etiam in vestibulum erat. Vivamus tristique orci et quam pharetra fringilla. </p>
+			<img src="http://lorempixel.com/200/200" />
+			<img src="http://lorempixel.com/200/200" />
+			<img src="http://lorempixel.com/200/200" />
+		</div>
+	</section>
+
+   
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.11.1.min.js"><\/script>')</script>
+	<script src="js/script.js"></script>
+
+</body>
+</html>
