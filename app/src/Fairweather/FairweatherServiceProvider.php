0c3940372613b4acf08ed8fd7d36e161c47b1965
Jordan Lane - Laptop <jordan@softsquared.co.uk>
2015-02-02 01:43:06 +0000

    Use actual facebook page id instead of album

--- app/src/Fairweather/FairweatherServiceProvider.php
+++ app/src/Fairweather/FairweatherServiceProvider.php
@@ -12,7 +12,7 @@ class FairweatherServiceProvider extends ServiceProvider {
     public function register()
     {
         $this->app->bind('Fairweather\Repositories\FacebookRepository', function() {
-            return new Repositories\FacebookRepository("1441126582824141", "315006852031986", "Et4dPA0rJeZ1aSx98M9p-4RXxUM");
+            return new Repositories\FacebookRepository("1441025676167565", "315006852031986", "Et4dPA0rJeZ1aSx98M9p-4RXxUM");
         });
         $this->app->bind('Fairweather\Helpers\FacebookRedirectLoginHelper');
     }
