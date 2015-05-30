c9839c0212a29ed248973c7f0f34f9afbc8e7d0b
Jordan Lane - Laptop <jordan@softsquared.co.uk>
2015-05-30 17:13:04 +0100

    Fix non-cdn jquery link

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -163,7 +163,7 @@
 
    
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.11.1.min.js"><\/script>')</script>
+	<script>window.jQuery || document.write('<script src="/js/libs/jquery-1.11.1.min.js"><\/script>')</script>
 	<script src="js/script.js"></script>
 
 </body>
