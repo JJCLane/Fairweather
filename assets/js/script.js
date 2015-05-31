2849fb616f834653ebe1ea655486c81d0a2097d2
Jordan Lane - Laptop <jordan@softsquared.co.uk>
2015-05-31 04:06:40 +0100

    Add smooth scroll and form javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,4 +19,47 @@ $(function(){
 			$('#toggleMenu').trigger('click');
 		}
 	});
+
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	});
+
+	// trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+	if (!String.prototype.trim) {
+		(function() {
+			// Make sure we trim BOM and NBSP
+			var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+			String.prototype.trim = function() {
+				return this.replace(rtrim, '');
+			};
+		})();
+	}
+
+	$('.input__field').each(function() {
+
+		// Checkif the input is already filled
+		if( this.value.trim() !== '' ) {
+			$(this).parent().addClass('input--filled');
+		}
+
+	});
+
+	$('.input__field').on('focus', function() {
+		$(this).parent().addClass('input--filled');
+	});
+
+	$('.input__field').on('blur', function() {
+		if( this.value.trim() === '' ) {
+			$(this).parent().removeClass('input--filled');
+		}
+	});
 });
